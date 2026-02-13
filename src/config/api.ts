95b66003cfdb02434a0ecde3ea94f3418808c600
meko568 <mohammedelbardan82@gmail.com>
2026-02-13 20:49:30 +0200

    updated

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
+const API_BASE_URL = 'https://commercebackend-production-68d6.up.railway.app/';
 
 export const API_URL = API_BASE_URL;
 
